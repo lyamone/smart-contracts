b4e5e1016d5fb0abea656b80606d9fd4e1c5939c
Lucas <lucas.yamone@gmail.com>
2022-02-15 11:49:26 +0100

    Project Setup

---
+++ scripts/deploy.ts
@@ -0,0 +1,32 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+import { ethers } from "hardhat";
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  const Test = await ethers.getContractFactory("Test");
+  const test = await Test.deploy();
+
+  await test.deployed();
+
+  const Problem = await ethers.getContractFactory("Problem");
+  const problem = await Problem.deploy(1, 1);
+
+  await problem.deployed();
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
